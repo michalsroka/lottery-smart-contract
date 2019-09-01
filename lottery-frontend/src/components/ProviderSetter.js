757280ce78dbbd9936a3867005ec91e03f234e0d
michalsroka <rac.michal.s@gmail.com>
2019-09-01 23:41:16 +0200

    Add coin contracts

--- lottery-frontend/src/components/ProviderSetter.js
+++ lottery-frontend/src/components/ProviderSetter.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Web3 from "web3";
 
+import HelloWorld from "../contracts/HelloWorld.json";
+import MetaCoin from "../contracts/MetaCoin.json"
+import TruffleContract from "truffle-contract";
+
 const ProviderSetter = props => {
   if (window.ethereum) {
     const ethereum = window.ethereum;
@@ -11,6 +15,26 @@ const ProviderSetter = props => {
       const defaultAccount = account[0];
       web3Provider.eth.defaultAccount = defaultAccount;
       console.log("Default account: " + defaultAccount);
+
+      const HelloWorldContract = TruffleContract(HelloWorld);
+      HelloWorldContract.setProvider(web3Provider.currentProvider);
+      HelloWorldContract.defaults({ from: web3Provider.eth.defaultAccount });
+
+      HelloWorldContract.deployed().then(hwc => {
+        console.log("Executing sayHello func in HWcontract...");
+        console.log(hwc.sayHello());
+      });
+
+      const MetaCoinContract = TruffleContract(MetaCoin);
+      MetaCoinContract.setProvider(web3Provider.currentProvider);
+      MetaCoinContract.defaults({ from: web3Provider.eth.defaultAccount });
+
+      MetaCoinContract.deployed().then((coinContract) => {
+          const address = '0x7ef1643074f34d772ee759ba70b5f38271e49c1c';
+          
+          console.log(coinContract);
+          coinContract.sendCoin(address, BigInt('100000000000000000'));
+      });
     });
 
     /* make web3Provider available to your entire app now */
